b3eae02d6f78c8988c6a3859ef0f9650e605e9d9
Janysh <tynybekovjanyshbek@gmail.com>
2023-07-04 22:41:01 +0600

    todo2

--- apps/todo/admin.py
+++ apps/todo/admin.py
@@ -4,4 +4,4 @@ from apps.todo.models import ToDo
 @admin.register(ToDo)
 class ToDoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'is_completed', 'created_at', 'image')
-    list_filter = ('title', 'description', 'is_completed', 'created_at', 'image')
+    list_filter = ('title', 'description', 'is_completed', 'created_at', 'image') 
