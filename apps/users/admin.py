13c0378de3dfe9697191fe27cab45d0620d47180
Janysh <tynybekovjanyshbek@gmail.com>
2023-07-04 22:42:21 +0600

    todo9

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -6,3 +6,4 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name', 'phone_number', 'age', 'date_joined')
     search_fields = ('username', 'first_name', 'last_name', 'phone_number', 'age', 'date_joined')
     list_per_page = 20
+ 
