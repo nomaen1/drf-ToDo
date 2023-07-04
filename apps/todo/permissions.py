721ccd4454f8ef0fe618e67f35c71ff5161fcb8b
Janysh <tynybekovjanyshbek@gmail.com>
2023-07-04 22:41:34 +0600

    todo5

--- apps/todo/permissions.py
+++ apps/todo/permissions.py
@@ -2,4 +2,4 @@ from rest_framework import permissions
 
 class IsOwnerPermissions(permissions.BasePermission):
     def has_object_permission(self, request,  obj):
-        return bool(obj.user.pk == request.user.pk)
+        return bool(obj.user.pk == request.user.pk) 
